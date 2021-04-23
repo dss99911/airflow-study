090682912a940c787bbc7e5643fc1bf60a6056da
Hyun <hyun@balancehero.com>
2021-04-23 11:44:57 +0900

    add

--- third_party/emr.py
+++ third_party/emr.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # https://airflow.apache.org/docs/apache-airflow-providers-amazon/stable/operators/emr.html
+import logging
+from datetime import datetime
 
+import boto3
+from airflow.models import Variable
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.providers.amazon.aws.operators.emr_add_steps import EmrAddStepsOperator
+from airflow.providers.amazon.aws.operators.emr_create_job_flow import EmrCreateJobFlowOperator
 from airflow.providers.amazon.aws.sensors.emr_step import EmrStepSensor
 from operators.dags import *
 
@@ -51,6 +57,7 @@ def add_emrfs_clean_steps(name, action='CONTINUE'):
         }
     }]
 
+
 def add_cluster_resize_steps(name, task_id, task_count, action='CONTINUE'):
     return [{
         'Name': name,
@@ -63,6 +70,7 @@ def add_cluster_resize_steps(name, task_id, task_count, action='CONTINUE'):
             ]
         }
     }]
+
 
 def add_cluster_instance_fleet_resize_steps(name, cluster_id, instance_fleet_id,
                                             target_on_demand_capacity, target_spot_capacity, action='CONTINUE'):
@@ -115,3 +123,48 @@ sensor_2 = EmrStepSensor(
 )
 
 step_1 >> sensor_1 >> step_2 >> sensor_2
+
+create_cluster = EmrCreateJobFlowOperator(
+    task_id='create_cluster',
+    aws_conn_id='aws_default',
+    emr_conn_id='emr_default',
+    email_on_failure=True,
+    dag=dag
+)
+
+
+def get_spot_instance_fleet_id(cluster_id):
+    emr_client = boto3.client('emr', region_name=Variable.get('aws_default_region'))
+    response = emr_client.list_instance_fleets(ClusterId=cluster_id)
+
+    for instance_fleet in response['InstanceFleets']:
+        if instance_fleet["InstanceFleetType"] == "TASK":
+            return instance_fleet["Id"]
+
+
+def dagrun_trigger(context, dag_run_obj):
+    cluster_id = context['ti'].xcom_pull('create_cluster', key='return_value')
+    instance_fleet_id = get_spot_instance_fleet_id(cluster_id)
+    _date = datetime.strptime(context['ds'], '%Y-%m-%d') + timedelta(days=1)
+    date = _date.strftime('%Y-%m-%d')
+    pdate = _date.strftime('%Y/%m/%d')
+    logging.info('cluster_id, date, pdate', cluster_id, date, pdate)
+    dag_run_obj.payload = {
+        'cluster_id': cluster_id,
+        'instance_fleet_id': instance_fleet_id,
+        'date': date,
+        'pdate': pdate
+    }
+    return dag_run_obj
+
+
+trigger = TriggerDagRunOperator(
+    task_id='trigger',
+    trigger_dag_id="subdag-id",
+    python_callable=dagrun_trigger,
+    sla=timedelta(minutes=20),
+    email_on_failure=True,
+    dag=dag
+)
+
+create_cluster >> trigger
